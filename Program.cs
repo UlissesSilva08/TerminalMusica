4abfb84a9a22ed4c2a0cec634696cac8cbceb1a8
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-26 20:18:33 -0300

    Inserindo a letra da música no comando WriteLine, mudança de cor para cadaverso e comando ReadKey para iniciar o programa

---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace TerminalMusica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Living a Boy's Adventure Tale - A-HA");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Tecle para iniciar ;)");
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            Console.WriteLine("Introdução");
+
+            Console.WriteLine("I've fixed my dwelling for the night");
+            Console.WriteLine("Lights in pairs come passing by where do I hide?");
+            Console.WriteLine("I need some time now on my own");
+            Console.WriteLine("Leave my loneliness alone");
+            Console.WriteLine("To lick my wounds");
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Pré refrão");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Night has found me just in time to help me close my");
+            Console.WriteLine("eyes here one more time");
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("Refrão");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("In so many ways");
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("For so many days");
+            Console.WriteLine("I'm living a boy's adventure tale");
+            Console.WriteLine("Can't escape, if I wanted to");
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("I may be dreaming but I feel awake");
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            Console.WriteLine("Verso");
+
+            Console.WriteLine();
+
+            Console.WriteLine("I've been lost in so many places");
+            Console.WriteLine("Seeked love in so many faces");
+            Console.WriteLine("A change of weather, the rain pours down");
+            Console.WriteLine("My head in hands, pressed to the ground");
+            Console.WriteLine("And where am I supposed to go now");
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("Refrão");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("In so many ways");
+            Console.WriteLine("Living a boy's adventure tale (aah)");
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("A voice I hear");
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("Singing a lullaby fine for me");
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("Because of you");
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("Ponte");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("Oh you know it's true");
+            Console.WriteLine("Living a boy's adventure tale");
+            Console.WriteLine("For so many days");
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Coda");
+
+            Console.WriteLine();
+
+            Console.WriteLine("I love you");
+            Console.WriteLine("I love you");
+            Console.WriteLine("So many ways");
+
+            Console.WriteLine();
+
+            Console.WriteLine("A ahaa!");
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+
+        }
+    }
+}
